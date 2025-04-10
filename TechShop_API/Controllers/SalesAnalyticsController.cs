0df13433f9d9236b14d2d339f6aa1ec8667d052b
AtakanDelikan <atakan798@gmail.com>
2025-04-10 20:36:34 +0200

    sales analytics controller rework

--- TechShop_API/Controllers/SalesAnalyticsController.cs
+++ TechShop_API/Controllers/SalesAnalyticsController.cs
@@ -20,74 +20,70 @@ namespace TechShop_API.Controllers
         public async Task<ActionResult<ApiResponse>> SalesAnalytics()
         {
             var rng = new Random();
-            // Generate time series data (flat arrays)
-
-            var revenueOverTime = GenerateYearlyTimeSeries(rng, 100, 300, 150, 350);
-            var ordersOverTime = GenerateYearlyTimeSeries(rng, 1, 10, 3, 12);
 
             _response.Result = (new
             {
                 totalRevenue = new Dictionary<string, decimal>
                 {
-                    ["2021"] = 125430m,
-                    ["2022"] = 158920m,
-                    ["2023"] = 194560m,
-                    ["2024"] = 227380m
+                    ["2021"] = 326485.73m,
+                    ["2022"] = 451972.64m,
+                    ["2023"] = 598321.47m,
+                    ["2024"] = 743158.29m
                 },
                 totalOrders = new Dictionary<string, int>
                 {
-                    ["2021"] = 213,
-                    ["2022"] = 261,
-                    ["2023"] = 279,
-                    ["2024"] = 317
+                    ["2021"] = 684,
+                    ["2022"] = 912,
+                    ["2023"] = 1085,
+                    ["2024"] = 1243
                 },
                 totalItemsSold = new Dictionary<string, int>
                 {
-                    ["2021"] = 842,
-                    ["2022"] = 1287,
-                    ["2023"] = 1753,
-                    ["2024"] = 2142
+                    ["2021"] = 1783,
+                    ["2022"] = 2347,
+                    ["2023"] = 2891,
+                    ["2024"] = 3245
                 },
                 uniqueCustomers = new Dictionary<string, int>
                 {
-                    ["2021"] = 83,
-                    ["2022"] = 131,
-                    ["2023"] = 207,
-                    ["2024"] = 246
+                    ["2021"] = 512,
+                    ["2022"] = 693,
+                    ["2023"] = 821,
+                    ["2024"] = 947
                 },
                 topSellingCategories = new Dictionary<string, int>
                 {
-                    ["Keyboards"] = 2235,
-                    ["PCs"] = 1247,
-                    ["Monitors"] = 992,
-                    ["Laptops"] = 815,
-                    ["Mice"] = 735
+                    ["Headphones"] = 864,
+                    ["Keyboards"] = 752,
+                    ["Cables"] = 687,
+                    ["Mice"] = 604,
+                    ["Laptops"] = 559
                 },
                 topRevenueByCategories = new Dictionary<string, decimal>
                 {
-                    ["Keyboards"] = 4120m,
-                    ["PCs"] = 3475m,
-                    ["Monitors"] = 3350m,
-                    ["Laptops"] = 2975m,
-                    ["Mice"] = 2080m
+                    ["Laptops"] = 278413.67m,
+                    ["Smartphones"] = 189402.56m,
+                    ["Monitors"] = 122850.88m,
+                    ["Tablets"] = 93410.21m,
+                    ["Desktops"] = 71465.44m
                 },
-                revenueOverTime,
-                ordersOverTime,
+                revenueOverTime = new Dictionary<string, decimal[]>
+                {
+                    ["2021"] = GenerateDailyValues(rng, 365, 350, 1800),
+                    ["2022"] = GenerateDailyValues(rng, 365, 400, 2200),
+                    ["2023"] = GenerateDailyValues(rng, 365, 500, 2800),
+                    ["2024"] = GenerateDailyValues(rng, 366, 600, 3200) // Leap year
+                },
+                ordersOverTime = new Dictionary<string, decimal[]>
+                {
+                    ["2021"] = GenerateDailyValues(rng, 365, 0, 5),
+                    ["2022"] = GenerateDailyValues(rng, 365, 1, 6),
+                    ["2023"] = GenerateDailyValues(rng, 365, 1, 7),
+                    ["2024"] = GenerateDailyValues(rng, 366, 2, 8) // Leap year
+                },
             });
 
             return _response;
-        }
-
-
-        private Dictionary<string, decimal[]> GenerateYearlyTimeSeries(Random rng, int minOld, int maxOld, int minRecent, int maxRecent)
-        {
-            return new Dictionary<string, decimal[]>
-            {
-                ["2021"] = GenerateDailyValues(rng, 365, minOld, maxOld),
-                ["2022"] = GenerateDailyValues(rng, 365, minOld, maxOld),
-                ["2023"] = GenerateDailyValues(rng, 365, minOld + 20, maxOld + 30),
-                ["2024"] = GenerateDailyValues(rng, 366, minRecent, maxRecent) // Leap year
-            };
         }
 
         private decimal[] GenerateDailyValues(Random rng, int days, int min, int max)
